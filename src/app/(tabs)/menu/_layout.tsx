a52e662f01f470144bad7a6209ede76674520257
abdulmateen <abdulmateen@sleepwellsystem.com>
2024-03-24 17:30:07 +0500

    update cart

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,9 +1,36 @@
-import { Stack } from "expo-router";
+import Colors from "@/src/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { Pressable, Text, View } from "react-native";
 
 export default function MenuStackedComponent() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ title: "Menu" }} />
+            <Stack.Screen
+                name="index"
+                options={{
+                    title: "Menu",
+                    headerRight: () => (
+                        <Link href="/cart" asChild>
+                            <Pressable>
+                                {
+                                    ({ pressed }) => {
+                                        return (
+                                            <FontAwesome
+                                                name="shopping-cart"
+                                                size={25}
+                                                color={Colors.light.tint}
+                                                style={{ marginRight: 10, opacity: pressed ? 0.5 : 1 }}
+                                            />
+                                        )
+                                    }
+                                }
+                            </Pressable>
+                        </Link>
+                    )
+                }}
+
+            />
         </Stack>
     )
 }
